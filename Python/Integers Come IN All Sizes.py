f5166a6db1712f6823e4857b7cd559a7ac276f95
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-17 00:54:16 +0530

    Create Integers Come IN All Sizes.py

---
+++ Python/Integers Come IN All Sizes.py
@@ -0,0 +1,19 @@
+'''Output Format
+a**b+c**d
+Print the result of  on one line.
+
+Sample Input
+
+9
+29
+7
+27
+Sample Output
+
+4710194409608608369201743232  '''
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(a**b+c**d)
