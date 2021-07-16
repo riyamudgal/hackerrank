9f7ddf26eecb67d165af79cd9fe372eb9ed7b688
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-17 00:59:03 +0530

    Create Power- mod Power.py

---
+++ Python/Power- mod Power.py
@@ -0,0 +1,17 @@
+'''Sample Input
+
+3
+4
+5
+Sample Output
+
+81
+1'''
+
+a=int(input())
+b=int(input())
+m=int(input())
+print(pow(a,b))
+print(pow(a,b,m))
+
+'''The pow() function returns the value of x to the power of y (xy). If a third parameter is present, it returns x to the power of y, modulus z.'''
