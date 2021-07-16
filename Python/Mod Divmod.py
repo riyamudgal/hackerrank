534e7006f00d3937d073f350b1794512c4b386bf
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-17 00:56:17 +0530

    Create Mod Divmod.py

---
+++ Python/Mod Divmod.py
@@ -0,0 +1,19 @@
+'''
+Sample Input
+
+177
+10
+Sample Output
+
+17
+7
+(17, 7)'''
+
+
+a=int(input())
+b=int(input())
+print(a//b)
+print(a%b)
+print(divmod(a,b))
+
+'''The divmod() method in python takes two numbers and returns a pair of numbers consisting of their quotient and remainder. '''
