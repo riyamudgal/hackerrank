0a5507761244994c6b8692cac480aef5aa0715ee
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-19 00:15:44 +0530

    Create What's Your Name?.py

---
+++ Python/What's Your Name?.py
@@ -0,0 +1,16 @@
+'''Sample Input 0
+
+Ross
+Taylor
+Sample Output 0
+
+Hello Ross Taylor! You just delved into python.
+'''
+
+def print_full_name(first, last):
+      print("Hello "+first+" "+last+"! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
