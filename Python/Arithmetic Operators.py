f6526bf4e05b90677866190207cfc0afe1c8e0a3
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-19 00:13:39 +0530

    Create Arithmetic Operators.py

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,24 @@
+'''
+Sample Input 0
+
+3
+2
+Sample Output 0
+
+5
+1
+6
+Explanation 0
+3+2=5
+3-2=1
+3*2=6
+'''
+
+ a=int(input())
+ b=int(input())
+ c=a+b
+ print(c)
+ c=a-b
+ print(c)
+ c=a*b
+ print(c)
