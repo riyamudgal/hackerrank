a8cb2fbb9f26b91bc73d9119c1bedf12ab997c0b
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-30 20:40:14 +0530

    Create Input and Output.cpp

---
+++ CPP/Course/Input and Output.cpp
@@ -0,0 +1,23 @@
+/*Sample Input
+
+1 2 7
+Sample Output
+
+10
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {  
+   
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
+    return 0;
+}
