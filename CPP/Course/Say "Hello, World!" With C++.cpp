47263d2dc63c41404f10c2809f723ecb8a1b71a5
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-30 20:38:34 +0530

    Create Say "Hello, World!" With C++.cpp

---
+++ CPP/Course/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,19 @@
+/*Output Format
+
+Print Hello, World! to stdout.
+
+Sample Output
+
+Hello, World!
+*/
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    string str="Hello, World!";
+    cout<<str;
+    return 0;
+}
